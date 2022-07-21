17e03812cdbbe3d762fc6fc980923d3839fccef4
Scenaristeur <scenaristeur@gmail.com>
2022-07-22 00:59:24 +0200

    Initial dist subtree commit

---
+++ dist/js/about.34f69e93.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkod"]=self["webpackChunkod"]||[]).push([[443],{5399:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var s=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],a=u(1001),l={},r=(0,a.Z)(l,s,e,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.34f69e93.js.map
